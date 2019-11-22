601fcce8f3fc54652afc0729dadf64cd08389a28
Pelekh Ruslan <yura39@i.ua>
2019-11-22 19:55:21 +0200

    50% complite

--- js/common.js
+++ js/common.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+    
+    $("#portfolio-grid").mixItUp();
+
+    $(".s-portfolio li").click(function(){
+        $(".s-portfolio li").removeClass("active");
+            $(this).addClass("active");
+});
 
     $('.popap').magnificPopup({type:'image'});
+    $('.popap-content').magnificPopup({type:'inline'});
 
     $(".top-text h1").animated("fadeInDown", "fadeOutUp");
     $(".top-text p, .section-header").animated("fadeInUp", "fadeInDown");
@@ -8,6 +16,9 @@ $(document).ready(function() {
     $(".animation-1").animated("fadeInLeft", "fadeOutLeft");
     $(".animation-3").animated("fadeInRight", "fadeOutRight");
     $(".animation-2").animated("flipInY", "flipOutY");
+
+    $(".left .resume-items").animated("fadeInLeft","fadeOutDown")
+    $(".right .resume-items").animated("fadeInRight", "fadeOutDown");
 
     function heigthDetect(){
         $(".main-heaer").css("height", $(window).height());
@@ -17,13 +28,6 @@ $(document).ready(function() {
     heigthDetect();   
     });
 
-    function heigthDetectTwo(){
-        $("#about").css("height", $(window).height());
-    };
-    heigthDetectTwo();
-    $(window).resize(function(){
-    heigthDetectTwo();   
-    });
 
 $(".toggle-menu").click(function(){
     if($(".top-menu").is(":visible")){
@@ -35,7 +39,12 @@ $(".toggle-menu").click(function(){
         $(".top-menu").fadeIn("600"),
         $(".top-menu li a").addClass("fadeInUp animated")
     );
-    });	
+    });
+$(".portfolio-item").each(function(i){
+    $(this).find("a").attr("href","#work-" + i);
+    $(this).find(".port-discr").attr("id", "work-"+ i);
+});
+    
 });
 $(window).load(function() { 
 	$(".loader-inner").fadeOut(); 
